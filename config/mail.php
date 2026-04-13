a3dd639f3853bb88182bd73e07885c9f20ac5e36
L-U-P-I-N <skjccm@gmail.com>
2026-04-13 11:48:58 +0300

    ههه

--- config/mail.php
+++ config/mail.php
@@ -111,7 +111,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'skjccm@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
     ],
 
